b4a5af1d50d66f5752982004bf42d74e5949229e
KityLinn <87649871+KityLinn@users.noreply.github.com>
2024-02-13 14:59:41 +0100

    initial build

---
+++ routes/posts.mjs
@@ -0,0 +1,57 @@
+const express = require("express");
+
+// recordRoutes is an instance of the express router.
+// We use it to define our routes.
+// The router will be added as a middleware and will take control of requests starting with path /listings.
+const recordRoutes = express.Router();
+
+// This will help us connect to the database
+const dbo = require("../db/conn");
+
+// Get a list of 50 posts
+router.get("/", async (req, res) => {
+  let collection = await db.collection("posts");
+  let results = await collection.find({})
+    .limit(50)
+    .toArray();
+  res.send(results).status(200);
+});
+
+// Get a single post
+router.get("/:id", async (req, res) => {
+  let collection = await db.collection("posts");
+  let query = {_id: ObjectId(req.params.id)};
+  let result = await collection.findOne(query);
+  if (!result) res.send("Not found").status(404);
+  else res.send(result).status(200);
+});
+
+// Add a new document to the collection
+router.post("/", async (req, res) => {
+  let collection = await db.collection("posts");
+  let newDocument = req.body;
+  newDocument.date = new Date();
+  let result = await collection.insertOne(newDocument);
+  res.send(result).status(204);
+});
+
+// Update the post with a new comment
+router.patch("/comment/:id", async (req, res) => {
+  const query = { _id: ObjectId(req.params.id) };
+  const updates = {
+    $push: { comments: req.body }
+  };
+  let collection = await db.collection("posts");
+  let result = await collection.updateOne(query, updates);
+  res.send(result).status(200);
+});
+
+// Delete an entry
+router.delete("/:id", async (req, res) => {
+  const query = { _id: ObjectId(req.params.id) };
+  const collection = db.collection("posts");
+  let result = await collection.deleteOne(query);
+  res.send(result).status(200);
+});
+
+module.exports = recordRoutes;
